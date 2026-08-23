6e071a75f786d2ed8e748ed21b8ecd165f96259d
Xwhiteroom <126281958+Xwhiteroom@users.noreply.github.com>
2026-08-23 01:43:38 +0100

    Create xlsf.py

---
+++ xlsf.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""
+# Copyright (c) 2026 Syed Shakir Iqbal (Xwhiteroom)
+# SPDX-License-Identifier: MIT
+
+"""
+
+import argparse
+import os
+import shlex
+import subprocess
+from datetime import datetime
+
+p = argparse.ArgumentParser(description="LSF wrapper")
+p.add_argument("--cores", "-n", type=int, default=1, help="LSF core count")
+p.add_argument("--ram", type=int, default=10000, help="Memory in MB")
+p.add_argument("--project", "-P", default="pt-impl", help="LSF project pt-impl/sb-impl")
+p.add_argument("--queue", "-q", default="high", help="LSF queue")
+p.add_argument("--type", "-t", default="RHEL8_64", help="LSF Resource Machine")
+p.add_argument("--select", default="csbatch", help="LSF select constraint")
+p.add_argument("--output", help="Override log file path")
+p.add_argument("--logdir", default="~/trash", help="Log directory")
+p.add_argument("--logprefix", default="genie_", help="Log filename prefix")
+p.add_argument("--terminal", default="xterm", help="gnome/xterm")
+
+p.add_argument("--dry", action="store_true", help="Print command, do not submit")
+p.add_argument("cmd", nargs=argparse.REMAINDER, help="Command to run")
+
+args = p.parse_args()
+
+if args.terminal == "gnome":
+    title = f"XLSF_GT_{args.cores}x{args.ram // 1000}GB"
+    term_cmd = f"gnome-terminal --title={title}"
+elif args.terminal == "xterm":
+    title = f"XLSF_XT_{args.cores}x{args.ram // 1000}GB"
+    term_cmd = f"xterm -T {title}"
+else:
+    raise ValueError(f"Unsupported terminal: {args.terminal}")
+
+
+#if not args.cmd: p.error("No command specified. Use '-- <command>'")
+if args.cmd:
+    #cmd = (f"{term_cmd} "  f"-- bash -lc {shlex.quote(user_cmd)}")    
+    cmd = " ".join(shlex.quote(x) for x in args.cmd)
+else:
+    cmd = term_cmd
+
+logfile = (
+    os.path.expanduser(args.output)
+    if   args.output
+    else os.path.expanduser(f"{args.logdir}/{args.logprefix}{datetime.now():%Y%m%d}.log")
+)
+
+bsub_cmd = [
+    "lsf_bsub",
+    "-o", logfile,
+    "-n", str(args.cores),
+    "-q", args.queue,
+    "-P", args.project,
+    "-J", title,    
+    "-R",
+    f"rusage[mem={args.ram}] order[cpuf:-maxmem:-mem] "
+    f"select[(type=={args.type})&&({args.select})]",
+    cmd,
+]
+
+if args.dry:
+    print("<CMD> ", " ".join(shlex.quote(x) for x in bsub_cmd))    
+else:
+    print("<CMD> ", " ".join(shlex.quote(x) for x in bsub_cmd))    
+    subprocess.run(bsub_cmd, check=True)
